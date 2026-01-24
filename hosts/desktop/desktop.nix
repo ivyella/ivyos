302402cc09c4ba442dea7bcd1f1ae0d80f3aac20
Ivory <ivowory@protonmail.com>
2026-01-24 03:46:01 +0200

    initial nixos files for my main host. TODO: make a flake or something

---
+++ hosts/desktop/desktop.nix
@@ -0,0 +1,90 @@
+{ pkgs, ... }:
+
+{
+  nixpkgs.config.permittedInsecurePackages = [
+    "nexusmods-app-0.21.1"
+  ];
+
+  # Packages 
+  environment.systemPackages = with pkgs; [
+    # Core tools
+    kitty
+    fish
+    starship
+    fastfetch
+    eza
+    pavucontrol
+    keyd
+    openjdk21
+    playerctl
+    flatpak
+    micro
+    xdg-desktop-portal-gnome
+    glib
+    git 
+    codeberg-cli
+    firefox
+    stow
+
+    # Niri / Wayland stack
+    niri
+    waybar
+    swaynotificationcenter
+    fuzzel
+    swww
+    wlogout
+    matugen
+    xwayland-satellite
+    bibata-cursors
+    nwg-look
+
+    # Applications
+    nautilus
+    vesktop
+    vscodium
+    spotify
+    prismlauncher
+    steam
+    yazi
+    nexusmods-app
+
+    adwaita-icon-theme
+    gnome-themes-extra
+    adw-gtk3
+  
+    vitetris
+  ];
+
+  # Login manager
+ 
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd niri-session";
+        user = "greeter";
+        };
+      };
+     };
+
+  services.dbus.enable = true;
+  programs.dconf.enable = true;  	
+  # Portals
+  xdg.portal = {
+    enable = true;
+
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gnome
+    ];
+  };
+
+  # Steam
+  programs.steam.enable = true;
+
+  # Fish as default shell
+  programs.fish.enable = true;
+  users.defaultUserShell = pkgs.fish;
+
+  # Flatpak
+  services.flatpak.enable = true;
+}
--- profiles/material/.config/fish/config.fish
+++ profiles/material/.config/fish/config.fish
@@ -1,8 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions
     alias ls 'eza --icons'
+    alias syncdots 'stow -R'
 end
-
 # Initialize Starship prompt (NixOS-friendly)
 if type -q starship
     eval (starship init fish)

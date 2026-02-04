5d508f0f6822c5e812eb94f92677383732eb9ebd
ivy <ivowory@protonmail.com>
2026-02-04 09:11:17 +0200

    yet another cleanup and stuff idk

--- home/ivy.nix
+++ home/ivy.nix
@@ -1,42 +1,68 @@
 { config, pkgs, ... }:
+
+let
+  ivyRoot = "${config.home.homeDirectory}/ivyos";
+
+  configApps = [
+    "kitty"
+    "fish"
+    "fuzzel"
+    "niri"
+    "waybar"
+    "swaync"
+    "wlogout"
+    "starship"
+    "fastfetch"
+    "qt5ct"
+    "qt6ct"
+    "quickshell"
+    "zed"
+  ];
+
+  gtkFiles = [
+    "gtk-3.0/colors.css"
+    "gtk-3.0/gtk.css"
+    "gtk-4.0/colors.css"
+    "gtk-4.0/gtk.css"
+  ];
+
+in
 {
   home.username = "ivy";
   home.homeDirectory = "/home/ivy";
   home.stateVersion = "25.11";
-  
-  programs.home-manager.enable = true;
-  
-  home.file = {
-    # .config symlinks
-    ".config/kitty".source = config.lib.file.mkOutOfStoreSymlink "/home/ivy/ivyos/profiles/material/.config/kitty";
-    ".config/fish".source = config.lib.file.mkOutOfStoreSymlink "/home/ivy/ivyos/profiles/material/.config/fish";
-    ".config/fuzzel".source = config.lib.file.mkOutOfStoreSymlink "/home/ivy/ivyos/profiles/material/.config/fuzzel";
-    ".config/niri".source = config.lib.file.mkOutOfStoreSymlink "/home/ivy/ivyos/profiles/material/.config/niri";
-    ".config/waybar".source = config.lib.file.mkOutOfStoreSymlink "/home/ivy/ivyos/profiles/material/.config/waybar";
-    ".config/swaync".source = config.lib.file.mkOutOfStoreSymlink "/home/ivy/ivyos/profiles/material/.config/swaync";
-    ".config/wlogout".source = config.lib.file.mkOutOfStoreSymlink "/home/ivy/ivyos/profiles/material/.config/wlogout";
-    ".config/starship".source = config.lib.file.mkOutOfStoreSymlink "/home/ivy/ivyos/profiles/material/.config/starship";
-    ".config/fastfetch".source = config.lib.file.mkOutOfStoreSymlink "/home/ivy/ivyos/profiles/material/.config/fastfetch";
-    ".config/qt5ct".source = config.lib.file.mkOutOfStoreSymlink "/home/ivy/ivyos/profiles/material/.config/qt5ct";
-    ".config/qt6ct".source = config.lib.file.mkOutOfStoreSymlink "/home/ivy/ivyos/profiles/material/.config/qt6ct";
-    ".config/quickshell".source = config.lib.file.mkOutOfStoreSymlink "/home/ivy/ivyos/profiles/material/.config/quickshell";
-    ".config/zed".source = config.lib.file.mkOutOfStoreSymlink "/home/ivy/ivyos/profiles/material/.config/zed";
 
-    ".config/gtk-3.0/colors.css".source = config.lib.file.mkOutOfStoreSymlink "/home/ivy/ivyos/profiles/material/.config/gtk-3.0/colors.css";
-    ".config/gtk-3.0/gtk.css".source = config.lib.file.mkOutOfStoreSymlink "/home/ivy/ivyos/profiles/material/.config/gtk-3.0/gtk.css";
-    ".config/gtk-4.0/colors.css".source = config.lib.file.mkOutOfStoreSymlink "/home/ivy/ivyos/profiles/material/.config/gtk-4.0/colors.css";
-    ".config/gtk-4.0/gtk.css".source = config.lib.file.mkOutOfStoreSymlink "/home/ivy/ivyos/profiles/material/.config/gtk-4.0/gtk.css";
-  
+  programs.home-manager.enable = true;
+
+  # Symlinks for configs and themes
+  home.file = builtins.listToAttrs (
+    # .config apps
+    map (app: {
+      name = ".config/${app}";
+      value.source = config.lib.file.mkOutOfStoreSymlink "${ivyRoot}/profiles/material/.config/${app}";
+    }) configApps
+    ++
+    # GTK files
+    map (file: {
+      name = ".config/${file}";
+      value.source = config.lib.file.mkOutOfStoreSymlink "${ivyRoot}/profiles/material/.config/${file}";
+    }) gtkFiles
+    ++
     # .local/share symlinks
-    ".local/share/color-schemes".source = config.lib.file.mkOutOfStoreSymlink "/home/ivy/ivyos/profiles/material/.local/share/color-schemes";
-    ".local/share/PrismLauncher/themes".source = config.lib.file.mkOutOfStoreSymlink "/home/ivy/ivyos/profiles/material/.local/share/PrismLauncher/themes";
-  };
-  
+    [
+      { name = ".local/share/color-schemes"; value.source = config.lib.file.mkOutOfStoreSymlink "${ivyRoot}/profiles/material/.local/share/color-schemes"; }
+      { name = ".local/share/PrismLauncher/themes"; value.source = config.lib.file.mkOutOfStoreSymlink "${ivyRoot}/profiles/material/.local/share/PrismLauncher/themes"; }
+      { name = ".local/share/icons/poisonivy"; value.source = config.lib.file.mkOutOfStoreSymlink "${ivyRoot}/themes/icons/poisonivy"; }
+    ]
+  );
+
+  # Session variables
   home.sessionVariables = {
     XCURSOR_THEME = "Bibata-Modern-Classic";
     XCURSOR_SIZE = "20";
   };
-  
+
+  # GTK settings
   gtk = {
     enable = true;
     cursorTheme = {
@@ -44,8 +70,12 @@
       package = pkgs.bibata-cursors;
       size = 20;
     };
+    iconTheme = {
+      name = "poisonivy";
+    };
   };
-  
+
+  # Pointer/cursor
   home.pointerCursor = {
     gtk.enable = true;
     x11.enable = true;
@@ -54,29 +84,30 @@
     size = 20;
   };
 
+  # User services
   systemd.user.services = {
-  waybar = {
-    Unit = {
-      Description = "Waybar status bar";
-      PartOf = [ "graphical-session.target" ];
+    waybar = {
+      Unit = {
+        Description = "Waybar status bar";
+        PartOf = [ "graphical-session.target" ];
+      };
+      Service = {
+        ExecStart = "${pkgs.waybar}/bin/waybar";
+        Restart = "on-failure";
+      };
+      Install.WantedBy = [ "graphical-session.target" ];
     };
-    Service = {
-      ExecStart = "${pkgs.waybar}/bin/waybar";
-      Restart = "on-failure";
+
+    swaync = {
+      Unit = {
+        Description = "Sway Notification Center";
+        PartOf = [ "graphical-session.target" ];
+      };
+      Service = {
+        ExecStart = "${pkgs.swaynotificationcenter}/bin/swaync";
+        Restart = "on-failure";
+      };
+      Install.WantedBy = [ "graphical-session.target" ];
     };
-    Install.WantedBy = [ "graphical-session.target" ];
   };
-  
-  swaync = {
-    Unit = {
-      Description = "Sway Notification Center";
-      PartOf = [ "graphical-session.target" ];
-    };
-    Service = {
-      ExecStart = "${pkgs.swaynotificationcenter}/bin/swaync";
-      Restart = "on-failure";
-    };
-    Install.WantedBy = [ "graphical-session.target" ];
-  };
-};
 }

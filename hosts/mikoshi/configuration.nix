47b4e461392c7f9af6ec6854efe49e33b3ad014a
ivy <ivowory@protonmail.com>
2026-02-04 10:01:06 +0200

    Added todo and keyd config for my keyboard

--- hosts/mikoshi/configuration.nix
+++ hosts/mikoshi/configuration.nix
@@ -78,8 +78,46 @@
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
-  services.keyd.enable = true;
-
+  
+  services.keyd = {
+    enable = true;
+    keyboards = {
+      default = {
+        ids = [ "05ac:024f" ];
+        settings = {
+          main = {
+            brightnessdown = "f1";
+            brightnessup   = "f2";
+            scale          = "f3";
+            dashboard      = "f4";
+            kbdillumdown   = "f5";
+            kbdillumup     = "f6";
+            previoussong   = "f7";
+            playpause      = "f8";
+            nextsong       = "f9";
+            mute           = "f10";
+            volumedown     = "f11";
+            volumeup       = "f12";
+          };
+          fn = {
+            brightnessdown = "brightnessdown";
+            brightnessup   = "brightnessup";
+            scale          = "scale";
+            dashboard      = "dashboard";
+            kbdillumdown   = "kbdillumdown";
+            kbdillumup     = "kbdillumup";
+            previoussong   = "previoussong";
+            playpause      = "playpause";
+            nextsong       = "nextsong";
+            mute           = "mute";
+            volumedown     = "volumedown";
+            volumeup       = "volumeup";
+          };
+        };
+      };
+    };
+  };
+  
   fonts = {
   packages = with pkgs; [
     open-sans

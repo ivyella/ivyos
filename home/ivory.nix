465edaf092d0312eaad60882c09a2d58b42d1760
Ivory <ivowory@protonmail.com>
2026-02-04 02:42:48 +0200

    home manager tweaks (please work)

--- home/ivory.nix
+++ home/ivory.nix
@@ -8,27 +8,23 @@
   
   home.file = {
     # .config symlinks
-    ".config/kitty".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/ivyos/profiles/material/.config/kitty";
-    ".config/fish".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/ivyos/profiles/material/.config/fish";
-    ".config/fuzzel".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/ivyos/profiles/material/.config/fuzzel";
-    ".config/niri".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/ivyos/profiles/material/.config/niri";
-    ".config/waybar".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/ivyos/profiles/material/.config/waybar";
-    ".config/swaync".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/ivyos/profiles/material/.config/swaync";
-    ".config/wlogout".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/ivyos/profiles/material/.config/wlogout";
-    ".config/starship".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/ivyos/profiles/material/.config/starship";
-    ".config/fastfetch".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/ivyos/profiles/material/.config/fastfetch";
-    ".config/vesktop".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/ivyos/profiles/material/.config/vesktop";
-    ".config/gtk-3.0".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/ivyos/profiles/material/.config/gtk-3.0";
-    ".config/gtk-4.0".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/ivyos/profiles/material/.config/gtk-4.0";
-    ".config/qt5ct".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/ivyos/profiles/material/.config/qt5ct";
-    ".config/qt6ct".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/ivyos/profiles/material/.config/qt6ct";
-    ".config/quickshell".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/ivyos/profiles/material/.config/quickshell";
-    ".config/zed".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/ivyos/profiles/material/.config/zed";
-    ".config/systemd".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/ivyos/profiles/material/.config/systemd";
+    ".config/kitty".source = config.lib.file.mkOutOfStoreSymlink "/home/ivory/ivyos/profiles/material/.config/kitty";
+    ".config/fish".source = config.lib.file.mkOutOfStoreSymlink "/home/ivory/ivyos/profiles/material/.config/fish";
+    ".config/fuzzel".source = config.lib.file.mkOutOfStoreSymlink "/home/ivory/ivyos/profiles/material/.config/fuzzel";
+    ".config/niri".source = config.lib.file.mkOutOfStoreSymlink "/home/ivory/ivyos/profiles/material/.config/niri";
+    ".config/waybar".source = config.lib.file.mkOutOfStoreSymlink "/home/ivory/ivyos/profiles/material/.config/waybar";
+    ".config/swaync".source = config.lib.file.mkOutOfStoreSymlink "/home/ivory/ivyos/profiles/material/.config/swaync";
+    ".config/wlogout".source = config.lib.file.mkOutOfStoreSymlink "/home/ivory/ivyos/profiles/material/.config/wlogout";
+    ".config/starship".source = config.lib.file.mkOutOfStoreSymlink "/home/ivory/ivyos/profiles/material/.config/starship";
+    ".config/fastfetch".source = config.lib.file.mkOutOfStoreSymlink "/home/ivory/ivyos/profiles/material/.config/fastfetch";
+    ".config/qt5ct".source = config.lib.file.mkOutOfStoreSymlink "/home/ivory/ivyos/profiles/material/.config/qt5ct";
+    ".config/qt6ct".source = config.lib.file.mkOutOfStoreSymlink "/home/ivory/ivyos/profiles/material/.config/qt6ct";
+    ".config/quickshell".source = config.lib.file.mkOutOfStoreSymlink "/home/ivory/ivyos/profiles/material/.config/quickshell";
+    ".config/zed".source = config.lib.file.mkOutOfStoreSymlink "/home/ivory/ivyos/profiles/material/.config/zed";
     
     # .local/share symlinks
-    ".local/share/color-schemes".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/ivyos/profiles/material/.local/share/color-schemes";
-    ".local/share/PrismLauncher/themes".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/ivyos/profiles/material/.local/share/PrismLauncher/themes";
+    ".local/share/color-schemes".source = config.lib.file.mkOutOfStoreSymlink "/home/ivory/ivyos/profiles/material/.local/share/color-schemes";
+    ".local/share/PrismLauncher/themes".source = config.lib.file.mkOutOfStoreSymlink "/home/ivory/ivyos/profiles/material/.local/share/PrismLauncher/themes";
   };
   
   home.sessionVariables = {

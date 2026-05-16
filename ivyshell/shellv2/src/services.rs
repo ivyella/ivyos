2e912320c50a953d09b3e28633909a467020a3b6
ivy <ivowory@protonmail.com>
2026-05-17 01:31:28 +0200

    style(shell): auto-format codebase via cargo fmt

--- ivyshell/shellv2/src/services.rs
+++ ivyshell/shellv2/src/services.rs
@@ -1,8 +1,8 @@
-pub mod clock;
 pub mod audio;
-pub mod weather;
-pub mod night_light;
-pub mod network;
+pub mod battery;
 pub mod bluetooth;
 pub mod brightness;
-pub mod battery;
+pub mod clock;
+pub mod network;
+pub mod night_light;
+pub mod weather;
